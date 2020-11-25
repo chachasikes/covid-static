784c7c9727a1fdcf64b429b164f44de8dedc3845
Chacha Sikes <chachasikes@gmail.com>
2020-11-25 01:53:02 -0800

    update script to process Industry Guidance guidance docs.

--- src/extract-pdf-markdown.js
+++ src/extract-pdf-markdown.js
@@ -32,16 +32,18 @@ files.forEach(file => {
     }
 });
 
-// Get specific types of PDFs.
-// console.log(arrayOfFiles);
-let checklistFiles = arrayOfFiles.filter((file) => {
-  return file.includes('checklist-');
+let guidanceFiles = arrayOfFiles.filter((file) => {
+  if (file.match(/.pdf$/i)) {
+  return file.includes('guidance-');
+  } 
+  return false;
 });
-
-checklistFiles = checklistFiles.slice(0,3); // Sample
-// console.log(checklistFiles);
-
-// Read all files of a specific type.
+let checklistFiles = arrayOfFiles.filter((file) => {
+  if (file.match(/.pdf$/i)) {
+  return file.includes('checklist-');
+  } 
+  return false;
+});
 checklistFiles.forEach(checklistFile => {
   // Create a pdf parser.
   let pdfParser = new PDFParser(this,1);
@@ -52,27 +54,79 @@ checklistFiles.forEach(checklistFile => {
     // console.log(pdfData)
     // Read file contents and build plain text file as markdown.
     // Pass the file a custom formatter for the raw text.
-    let markdownDoc = getMarkdown({formatter: formatterChecklist, pdfParser});
-    console.log(markdownDoc);
-
+    let markdownDoc = getMarkdown({formatter: formatterGuidance, pdfParser});
     let markdownFileName = checklistFile.replace(/.pdf/i, '.md');
     // console.log(markdownFileName);
     fs.writeFile(`${outputFolderPath}/${markdownFileName}`, markdownDoc, err => {if (err) console.error(err); else console.log('file created.');});
-
     return;
-
   });
 })
 
-const formatterChecklist = (text) => {
+console.log(guidanceFiles.length); // 569 files
+guidanceFiles = guidanceFiles.slice(551,600);
+// Last processed 11/25-2020, need to review the output.
+
+// @TODO Make the batch formatting smarter.
+// Spinning out too many processing requests at once hangs script.
+const formatterGuidance = (text) => {
   text = text.replace(//g, "▢");
   return text;
 }
 
-let guidanceFiles = arrayOfFiles.filter((file) => {
-  return file.includes('guidance-');
-});
-// console.log(guidanceFiles);
+const historicalConversions = () => {
+  // archivalFolder, files saved as naming convention eg. guidance-transit-rail--ar.v1.7.pdf 
+  // or guidance-file--en.2020-07-02.pdf 
+  // host these files in folder called 'archive'
+  // move archived files, renamed, to the archive folder.
+  // process these the same way as the current files.
+}
+
+const automation = () => {
+  // Since the files won't change, we can store a setting file with the date last updated for the md files, or detect it.
+  // If it's in text, it may be easier to read and refer to.
+  // We can do batches automatically and store.
+  // This needs a function script & a github action.
+  // Before we do that, we need better batching & also to test the processing on our server.
+}
+
+const buildMetadata = () => {
+  // Currently we have document information from the pdf
+  // We can see the publish date easily in the markdown, and also check the differences
+  // It could be ideal if the PDF files have the right version & publish date, as well as canonical paths
+  // We could augment the .md file with additional metadata, in the header or in a final section at the end, or in another file
+  // called filename.meta.md or filename.meta.json
+
+}
+
+guidanceFiles.forEach(checklistFile => {
+  // Create a pdf parser.
+  let pdfParser = new PDFParser(this,1);
+  // Load the file.
+  pdfParser.loadPDF(`${inputFolderPath}/${checklistFile}`);
+  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    // Read file contents and build plain text file as markdown.
+    // Pass the file a custom formatter for the raw text.
+    let markdownDoc = getMarkdown({formatter: formatterGuidance, pdfParser});
+    let markdownFileName = checklistFile.replace(/.pdf/i, '.md');
+    fs.writeFile(`${outputFolderPath}/${markdownFileName}`, markdownDoc, err => {if (err) console.error(err); else console.log('file created.');});
+    return;
+  });
+})
+
+const createSideBySideCsv = () => {
+  // same file name
+  // get text
+  // split by '.'
+  // create arrays
+    // file name
+        // lang
+  // read arrays
+  // CSV header, language
+  // save each key in all languages as a field
+  // handle fields
+  // save file as csv
+}
 
 const getMarkdown = ({formatter = null, pdfParser}) => {
   // Process date
@@ -108,43 +162,3 @@ const getMarkdown = ({formatter = null, pdfParser}) => {
   let markdownDoc = doc.join('\n');
   return markdownDoc;
 }
-
-
-
-
-
-
-
-
-
-
-// didn't work
-// pdfParser.on("pdfParser_dataReady", pdfData => {
-//     fs.writeFile(`${outputFolderPath}/checklist-agriculture--en.json`, JSON.stringify(pdfData));
-// });
-
-// let rawText = textbody.join(' ');
-// let replaceCheckbox = rawText.replace(/ï¿/gi, '&#9744;'); //▢
-// console.log(replaceCheckbox);
-
-// Title: 'COVID19 Agriculture and Livestock Checklist',
-// Author: 'State of California',
-// Subject: 'COVID-19 Industry Guidance - Car Dealerships and Rental Operators',
-// Keywords: 'COVID-19, checklist coronavirus, Agriculture and Livestock',
-// Creator: 'Microsoft® Word 2016',
-// Producer: 'Microsoft® Word 2016',
-// CreationDate: "D:20200702135430-07'00'",
-// ModDate: "D:20200702135430-07'00'"
-
-// let textbody = [];
-
-// pdfParser.PDFJS.pages.map((page) => {
-//     page.Texts.map((text) => {
-//         try {
-//             let string = unescape(text.R[0].T);
-//             textbody.push(string);
-//         } catch (err) {
-
-//         }
-//     });
-// });
