71f8bcffce43e63de06a7d5a14c13931538e2864
Chacha Sikes <chachasikes@gmail.com>
2020-12-23 15:56:19 -0800

    update script - category import much better

--- history/script/git-history.sh
+++ history/script/git-history.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
-headers="id,git_industry_category_key,git_pdf_template_type,git_filepath,git_filename,git_publishing_status,git_date_updated,git_commit_author,git_commit_subject,github_url,git_permalink,git_download_url,git_pdf_language,git_type_document,git_commit_hash,git_commit_hash_abbreviated,git_repository,git_change_type"
+# NOTE: make sure line endings are not CRLF
+# CSV headers
+headers="id,git_filename,git_industry_category_key,git_pdf_template_type,git_pdf_language,git_permalink,git_date_updated,github_url,git_publishing_status,git_commit_author,git_commit_subject,git_download_url,git_type_document,git_commit_hash,git_commit_hash_abbreviated,git_repository,git_filepath,git_change_type"
 repo="https://github.com/cagov/covid-static/"
+# CSV Filename
 filename="./history/csv/pdf-history.csv"
+# Delete old CSV file
 rm -rf ${filename}
+# Make new CSV file
 touch ${filename}
+# Insert headers into CSV file
 echo ${headers} >> ${filename}
+# Determine where in the git history to load data from
 startCommit="master"
+# Target folder
 folder="pdf"
+# Path to where these files are hosted on the web
 public_url="https://files.covid19.ca.gov/pdf/"
+# Show total commits in the history of this folder
 echo "Total Commits"
 git log --pretty=format:'' | wc -l
+# Split headers into an array
 IFS=',' read -r -a headersArray <<< "$headers"
-# --diff-filter=ACDMRT # https://git-scm.com/docs/git-log
-# git log master --pretty=format: --name-only --all --full-history --diff-filter=D -- pdf*uidance* pdf*hecklist* | sort - | sed '/^$/d'
-#for file in $(git log ${startCommit} --pretty=format: --name-only --all --full-history --diff-filter=A -- ${folder}*uidance* ${folder}*hecklist* | sort - | sed '/^$/d');
-
+# Build history
 get_history () {
+# Set the type of git status change we are retrieving (example A D M, Added, Deleted, Modified)
 type_change=$1
+# For each file
+# of all commits of current type of change (example: A - Added).
+# Load the full git history
+# Filtered by search parameters
+# @TODO abstract ${folder}*uidance* ${folder}*hecklist* (try again, did not work out the first time)
+# Sort results
+# Remove empty lines
 for file in $(git log ${startCommit} --pretty=format: --name-only --diff-filter=${type_change} --all --full-history -- ${folder}*uidance* ${folder}*hecklist* | sort - | sed '/^$/d');
 do
-    # echo ${file}
-    # for response in $git_response;
-    # do
-    #     type_change=${response[0]}
-    #     file=${response[1]}
-    # done
-
-
+    # Build format string for CSV data
     formatString=""
     for i in "${headersArray[@]}"
     do
-        # echo $i
         if [[ $i == "github_url" ]]
             then
                 formatString+="${repo}blob/%H/${file},"
@@ -49,6 +57,8 @@ do
                 odi_industry_category_key=${odi_industry_category_key//.pdf/}
                 odi_industry_category_key=${odi_industry_category_key//.PDF/}
                 odi_industry_category_key=${odi_industry_category_key//--ar/}
+                odi_industry_category_key=${odi_industry_category_key//--zh-Hans/}
+                odi_industry_category_key=${odi_industry_category_key//.download.zip/}
                 odi_industry_category_key=${odi_industry_category_key//--en/}
                 odi_industry_category_key=${odi_industry_category_key//--es/}
                 odi_industry_category_key=${odi_industry_category_key//--hmn/}
@@ -61,12 +71,68 @@ do
                 odi_industry_category_key=${odi_industry_category_key//--vi/}
                 odi_industry_category_key=${odi_industry_category_key//--zh-tw/}
                 odi_industry_category_key=${odi_industry_category_key//--zh-hans/}
+                odi_industry_category_key=${odi_industry_category_key//--zh-hant/}
                 odi_industry_category_key=${odi_industry_category_key//--zh-cn/}
-
-
+                odi_industry_category_key=${odi_industry_category_key//--pa/}
+                odi_industry_category_key=${odi_industry_category_key//-ar/}
+                odi_industry_category_key=${odi_industry_category_key//-es/}
+                odi_industry_category_key=${odi_industry_category_key//-ko/}
+                odi_industry_category_key=${odi_industry_category_key//-tl/}
+                odi_industry_category_key=${odi_industry_category_key//-zh-hans/}
+                odi_industry_category_key=${odi_industry_category_key//-zh-hant/}
+                odi_industry_category_key=${odi_industry_category_key//_tl/}
+                odi_industry_category_key=${odi_industry_category_key//_es/}
+                odi_industry_category_key=${odi_industry_category_key//-ES/}
+                odi_industry_category_key=${odi_industry_category_key//_Arabic/}
+                odi_industry_category_key=${odi_industry_category_key//_CH_Simplified/}
+                odi_industry_category_key=${odi_industry_category_key//_Tagalog/}
+                odi_industry_category_key=${odi_industry_category_key//_Korean/}
+                odi_industry_category_key=${odi_industry_category_key//_CH_Simplified/}
+                odi_industry_category_key=${odi_industry_category_key//_Vietnamese/}
+                odi_industry_category_key=${odi_industry_category_key//-vi/}
+                formatString+="\"${odi_industry_category_key}\","
+            elif [[ $file =~ .*"uidance".* ]];
+                then
+                odi_industry_category_key=${file//pdf\/guidance-/}
+                odi_industry_category_key=${odi_industry_category_key//.pdf/}
+                odi_industry_category_key=${odi_industry_category_key//.PDF/}
+                odi_industry_category_key=${odi_industry_category_key//--ar/}
+                odi_industry_category_key=${odi_industry_category_key//--zh-Hans/}
+                odi_industry_category_key=${odi_industry_category_key//.download.zip/}
+                odi_industry_category_key=${odi_industry_category_key//--en/}
+                odi_industry_category_key=${odi_industry_category_key//--es/}
+                odi_industry_category_key=${odi_industry_category_key//--hmn/}
+                odi_industry_category_key=${odi_industry_category_key//--hy/}
+                odi_industry_category_key=${odi_industry_category_key//--km/}
+                odi_industry_category_key=${odi_industry_category_key//--ko/}
+                odi_industry_category_key=${odi_industry_category_key//--ru/}
+                odi_industry_category_key=${odi_industry_category_key//--th/}
+                odi_industry_category_key=${odi_industry_category_key//--tl/}
+                odi_industry_category_key=${odi_industry_category_key//--vi/}
+                odi_industry_category_key=${odi_industry_category_key//--zh-tw/}
+                odi_industry_category_key=${odi_industry_category_key//--zh-hans/}
+                odi_industry_category_key=${odi_industry_category_key//--zh-hant/}
+                odi_industry_category_key=${odi_industry_category_key//--zh-cn/}
+                odi_industry_category_key=${odi_industry_category_key//--pa/}
+                odi_industry_category_key=${odi_industry_category_key//-ar/}
+                odi_industry_category_key=${odi_industry_category_key//-es/}
+                odi_industry_category_key=${odi_industry_category_key//-ko/}
+                odi_industry_category_key=${odi_industry_category_key//-tl/}
+                odi_industry_category_key=${odi_industry_category_key//-zh-hans/}
+                odi_industry_category_key=${odi_industry_category_key//-zh-hant/}
+                odi_industry_category_key=${odi_industry_category_key//_tl/}
+                odi_industry_category_key=${odi_industry_category_key//_es/}
+                odi_industry_category_key=${odi_industry_category_key//-ES/}
+                odi_industry_category_key=${odi_industry_category_key//_Arabic/}
+                odi_industry_category_key=${odi_industry_category_key//_CH_Simplified/}
+                odi_industry_category_key=${odi_industry_category_key//_Tagalog/}
+                odi_industry_category_key=${odi_industry_category_key//_Korean/}
+                odi_industry_category_key=${odi_industry_category_key//_CH_Simplified/}
+                odi_industry_category_key=${odi_industry_category_key//_Vietnamese/}
+                odi_industry_category_key=${odi_industry_category_key//-vi/}
                 formatString+="\"${odi_industry_category_key}\","
             else
-                formatString+="General,"
+                formatString+="Other,"
             fi
         elif [ $i == "git_filename" ];
             then
@@ -102,7 +168,7 @@ do
             if [[ $file =~ .*"-ko".* ]] || [[ $file =~ .*"_Korean".*  ]];
                 then
                 formatString+="Korean,"
-            elif [[ $file =~ .*"-Vietnamese".* ]] || [[ $file =~ .*"-vi".* ]];
+            elif [[ $file =~ .*"-Vietnamese".* ]] || [[ $file =~ .*"-vi".* ]] || [[ $file =~ .*"_Vietnamese".* ]];
                 then
                 formatString+="Vietnamese,"
             elif [[ $file =~ .*"-pa".* ]] || [[ $file =~ .*"_Punjabi".* ]];
@@ -113,11 +179,11 @@ do
                 formatString+="Arabic,"
             elif [[ $file =~ .*"-zh-Hans".* ]] || [[ $file =~ .*"-zh-ch".* ]] || [[ $file =~ .*"-zh-hans".* ]] || [[ $file =~ .*"_CH_Simplified".* ]];
                 then
-                formatString+="Chinese%20Simplified,"
-            elif [[ $file =~ .*"-zh-hant".* ]] || [[ $file =~ .*"-zh-Hant".*  ]] || [[ $file =~ .*"-zh-tw".*  ]];
+                formatString+="Chinese-Simplified,"
+            elif [[ $file =~ .*"-zh-hant".* ]] || [[ $file =~ .*"-zh-Hant".*  ]] || [[ $file =~ .*"-zh-tw".*  ]] || [[ $file =~ .*"-zh-cn".*  ]];
                 then
-                formatString+="Chinese%20Traditional,"
-            elif [[ $file =~ .*"_Tagalog".*  ]] || [[ $file =~ .*"-tl".*  ]];
+                formatString+="Chinese-Traditional,"
+            elif [[ $file =~ .*"_Tagalog".*  ]] || [[ $file =~ .*"-tl".*  ]] || [[ $file =~ .*"_tl".*  ]];
                 then
                 formatString+="Tagalog,"
             elif [[ $file =~ .*"--th".*  ]];
@@ -132,7 +198,7 @@ do
             elif [[ $file =~ .*"--hmn".*  ]];
                 then
                 formatString+="Hmong,"
-            elif [[ $file =~ .*"_Spanish".*  ]] || [[ $file =~ .*"-es".*  ]];
+            elif [[ $file =~ .*"_Spanish".*  ]] || [[ $file =~ .*"-es".*  ]] || [[ $file =~ .*"_es".*  ]] || [[ $file =~ .*"-ES".*  ]];
                 then
                 formatString+="Spanish,"
             elif [[ $file =~ .*"_Armenian".*  ]] || [[ $file =~ .*"-hy".*  ]];
@@ -152,7 +218,7 @@ do
             formatString+="PDF,"
         elif [ $i == "git_publishing_status" ];
             then
-            formatString+="\"Git%20history\","
+            formatString+="\"Git-History\","
         elif [ $i == "git_filepath" ];
             then
             formatString+="${file},"
